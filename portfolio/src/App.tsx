05a48bd5f978bc66fd3436f835e49d66e3a65154
Pierre Petillot <petill_p@etna-alternance.net>
2022-10-31 17:09:33 +0100

    environement installed

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/about/About.tsx
@@ -0,0 +1,14 @@
+function About() {
+    return (
+        <div className="about">
+        <h1>About</h1>
+        <p>
+            I'm a software engineer based in the San Francisco Bay Area. I enjoy
+            building web applications, and I'm currently working on a project to
+            help people find their next home.
+        </p>
+        </div>
+    );
+}
+
+export default About;
