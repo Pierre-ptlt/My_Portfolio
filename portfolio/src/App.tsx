bbd19381071de73101c589a9e77478199c700978
Pierre Petillot <petill_p@etna-alternance.net>
2022-11-01 16:53:04 +0100

    generic css

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
+import Services  from './components/services/Services';
 
 function App() {
   return (
     <div>
       <Header />
       <Nav />
+      <About />
+      <Experience />
+      <Services />
     </div>
   );
 }
